6b181713bb7a10f4542e60141086150ed7f70587
Erjon Lamy <erjon.lamy@outlook.fr>
2017-06-15 16:54:46 +0200

    AsyncTask

--- app/src/main/java/com/example/admin/dta_android_tp15_asynctask/MainActivity.java
+++ app/src/main/java/com/example/admin/dta_android_tp15_asynctask/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.admin.dta_android_tp15_asynctask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar bar;
+    Traitement traitement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bar = (ProgressBar) findViewById(R.id.progressBar);
+
+        traitement = new Traitement();
+        traitement.execute();
+
+
+
+    }
+
+    class Traitement extends AsyncTask<Void, Integer, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            int progress;
+            for (progress = 0; progress <= 100; progress++){
+                if (isCancelled()) break;
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+
+                }
+                publishProgress(progress);
+            }
+            return " , dong !";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            bar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), "Ding !" + result, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onCancelled() {
+            Toast.makeText(getApplicationContext(), "D'oh !", Toast.LENGTH_LONG).show();
+        }
     }
 }
